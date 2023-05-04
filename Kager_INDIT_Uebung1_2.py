0b38f0e9d7663ea2df54813be0ead4b47feb8f08
Mynameasusername <132549234+Mynameasusername@users.noreply.github.com>
2023-05-04 11:31:04 +0200

    Add files via upload

---
+++ Kager_INDIT_Uebung1_2.py
@@ -0,0 +1,18 @@
+
+ia=0
+i=0
+"""
+while i < 10:
+    i+=1
+    print (i)
+"""
+
+while ia == 0:
+    ia=int(input("Eingabe Zahl1: "))
+    if ia == 0:
+        print("Zahl 1 = 0 !, neue Zahl eingeben")
+    else:
+        print("Gar nicht mal so schlecht")
+    
+
+    
